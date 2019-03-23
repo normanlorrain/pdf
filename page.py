6210720942562acdaede99074808c5522832d2e4
Norman Lorrain <normanlorrain@gmail.com>
2019-03-23 14:34:13 -0600

    Removing test pdf file.  some work on contents of  Page object.

--- page.py
+++ page.py
@@ -4,14 +4,14 @@ class Page():
         self.parent = parent
         self.objectNumber = ds.nextObjectNumber()
         self.contentsObjectNumber = 0
-        self.resourcesString = 0 
-        
+        self.resourcesString = "<< >>" 
+            #    /Contents {self.contentsObjectNumber} 0 R
+
     def __repr__(self):
         return f"""{self.objectNumber} 0 obj
     << /Type /Page
        /Parent {self.parent} 0 R
        /MediaBox [0 0 612 792]
-       /Contents {self.contentsObjectNumber} 0 R
        /Resources {self.resourcesString}
 
     >>
